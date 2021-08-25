f183e2eff28ec3ce33e47e5ff1372d71afab5a2d
bryanserranogithub <75232089+bryanserranogithub@users.noreply.github.com>
2021-08-25 15:40:50 -0400

    Add files via upload

--- lognormaldistribution.py
+++ lognormaldistribution.py
@@ -1,12 +1,23 @@
+# Log-normal_distribution.py
+#
+# SAXS data modeling script for spherical micelles. 
+#
+# This script returns a scattering intensity function as a line of best fit
+# for experimental data. It uses a log-normal distribution to estimate values.
+#
+# Check the readme file or Jupyter Notebook for more information.
+
+# Import libraries handeling excel reads and modeling calculations.
+
 import math
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import xlrd
 from scipy.integrate import quad
+from scipy.stats import lognorm
 
 # Import the necessary excel spreadsheets
-sample_data = pd.read_csv('sample_data.csv')
 scatteringfunction = pd.read_csv('lognormalvalues.csv')
 workbook = xlrd.open_workbook('sample_data.xlsx')
 
@@ -16,57 +27,82 @@ worksheet = workbook.sheet_by_index(0)
 spreadsheet = [[worksheet.cell_value(i,j) for i in range (1, worksheet.nrows)]
                                           for j in range (0,3)]
 
-# Input Variables
+# Declare spherical micelle parameters 
 Volume_Fraction = 0.01
-R = 55*(10**8)
-Rmed = 60*(10**8)
-sigma = 0.2
-SLD_sphere = (3.262)*(10**8)
-SLD_solvent = (3.069)*(10**8)
+Rmed = 60 * (10 ** -8)
+polydispersity = 0.2
+SLD_sphere = 10.64 * (10 ** 10)
+SLD_solvent = 9.42 * (10 ** 10)
 deltaRho = SLD_sphere - SLD_solvent
-bkg = 0
+sigma = polydispersity * Rmed
+R = lognorm.rvs(s = sigma, loc = Rmed, scale = math.exp(np.log(Rmed)), size = 2000, random_state = 3 )
+bkg = 0.001
 
-# Output gaussian distribution for a sphere using polydispersity, R, and Rmed
+# Return estimate for spherical micelle radius using gaussian distribution
+# with polydispersity, R, and Ravg as variables.
 def logNormalDistribution(sigma, R, Rmed):
-    fR = (1/((sigma)*R*math.sqrt(2*math.pi)))*math.exp((-1/(2*(sigma**2)))*(np.log(R)-np.log(Rmed))**2)
-    print("fR ", fR)
-    return fR
+    return (1 / ((sigma) * R * math.sqrt(2 * math.pi))) * math.exp((-1 / (2 * (sigma ** 2))) * (np.log(R) - np.log(Rmed)) ** 2)
 
-# Output scattering amplitude for a sphere using q and R
+# Return scattering amplitude for a sphere using q and R.
 def scatteringAmplitude(q, R):
-    FqR = (3*(math.sin(q*R)-(q*R)*math.cos(q*R)))/((q*R)**3)
-    print("fqR ", FqR)
-    return FqR
+    return (3 * (math.sin(q * R)-(q * R) * math.cos(q * R))) / ((q * R) ** 3)
 
-# Output integral equation component from scattering intensity equation
+# Return integral equation component from scattering intensity equation.
 def integrand(q, R):
-    return logNormalDistribution(sigma, R, Rmed)*(R**6)*(scatteringAmplitude(q, R))**2
+    return logNormalDistribution(sigma, R, Rmed) * (R ** 6) * (scatteringAmplitude(q, R)) ** 2
 
+# Calculate the integral for each q and R value and multiply constant 
+# values to integral value.
 def scatteringFunction(Volume_Fraction, deltaRho, sigma, R, Rmed):
-    #gaussian = gaussianDistribution()
-    #amplitude = [scatteringAmplitude(spreadsheet, i, R) for i in range (1, worksheet.nrows)]
-    
-    # Calculate the integral value for each
+
     for i in range (1, worksheet.nrows):
         for j in range (0,1):
-            q = spreadsheet[0][i-1]
+            q = spreadsheet[0][i - 1] * (10 ** -8)
             #print(q)
-            integral = [quad(integrand, 0, np.inf, args=(q))] 
+            integral = [quad(integrand, 0, np.inf, args = (q))] 
             #print(integral[0][0])
-            qScatteringFunction = ((((4*math.pi)/3)**2)*Volume_Fraction*(deltaRho**2))*(integral[0][0])
+            qScatteringFunction = ((((4 * math.pi) / 3) ** 2) * Volume_Fraction * (deltaRho ** 2)) * (integral[0][0])
             print(qScatteringFunction)
+            
+    print("\nUpdate lognormalvalues.csv with the new values if needed.\n")
 
-# Plot two sets of miscelle data in a scatterplot
-def plotSphericalMicelles(data, data2):
+
+# Plot two sets of miscelle data in a scatterplot. 
+def plotSphericalMicelles():
+    logNormVals = pd.read_csv('logNormalValues.csv')
     plt.subplot(111)
-    plt.scatter(data2.q, data.Iq, s = 0.9, c = 'red')
-    plt.ylim(1*10**-2, 1*10**10)
-    plt.xlim(0.002, 0.700)
+    plt.scatter(logNormVals.x, logNormVals.y, s = 0.9, c = 'red')
+    plt.xlim(0.001, 10)
+    plt.ylim(1 * 10 ** -10, 1)
     plt.xscale('log')
     plt.yscale('log')
     plt.xlabel('q ($Å^{-1}$)', fontsize = 12)
     plt.ylabel('Intensity ($cm^{-1}$)', fontsize = 12)
     plt.grid(True)
 
+# Plot two sets of miscelle data in a histogram. 
+def plotLogNormalDistribution(R, sigma):
+    
+    # Plot line of best fit for R value distribution.
+    x = np.linspace(lognorm.ppf(0.01, sigma), lognorm.ppf(0.99, sigma), 1000)
+    fig, ax = plt.subplots(figsize = (16, 6))
+    rv = lognorm(sigma)
+    ax.plot(x, rv.pdf(x))
+    
+    # Plot histogram to show R value distribution.
+    r = lognorm.rvs(sigma, size = 1000)
+    ax.hist(r, bins = 275, density = True)
+
+    plt.show()
+    
+    # Display parameter values.
+    print("Parameters:")
+    print("\nmean (Ravg):", lognorm.mean(Rmed, sigma))
+    print("median (Ravg):", lognorm.median(Rmed, sigma))
+    print("var:", lognorm.var(Rmed, sigma))
+    print("std (Sigma):", lognorm.std(Rmed, sigma))
+
 scatteringFunction(Volume_Fraction, deltaRho, sigma, R, Rmed)
-plotSphericalMicelles(scatteringfunction, sample_data)
+
+plotSphericalMicelles()
+plotLogNormalDistribution(R, sigma)
